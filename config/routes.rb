329272d2d6beacb1920af7e6022da1103d15b572
elissaeng <elissaeng@gmail.com>
2021-09-08 11:26:01 -0700

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "entries", to: "entries#index"
+  get 'entries', to: 'entries#index'
+
+  root to: 'main#index'
 end
